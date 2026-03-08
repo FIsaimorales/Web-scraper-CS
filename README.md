933f0db2a950ac6e20881c31cbe22dd5dc106573
FIsaimorales <frankisaimorales@gmail.com>
2026-03-08 11:03:14 -0300

    First Commit: Cibersecurity OSINT scraper

---
+++ README.md
@@ -0,0 +1,34 @@
+# CVE Sentinel Scraper
+
+## Descripción
+Este es un proyecto de **automatización OSINT** (Open Source Intelligence) desarrollado para recolectar y centralizar información sobre las últimas vulnerabilidades publicadas. La herramienta realiza un web scraping de fuentes públicas para extraer datos críticos, permitiendo un monitoreo rápido de nuevas amenazas.
+
+Este proyecto forma parte de mi portafolio como estudiante de Licenciatura en Ciberseguridad, Resaltando:
+# Automatización con Python.
+# Recolección de inteligencia de amenazas.
+# Manejo y estructuración de datos.
+
+## Funcionalidades
+* **Extracción Automática:** Obtiene los últimos IDs de CVE, descripciones y niveles de severidad.
+* **Exportación de Datos:** Guarda los resultados en formato `.csv` para su posterior análisis en herramientas de gestión de vulnerabilidades.
+* **Filtrado Inteligente:** Capacidad para identificar vulnerabilidades críticas de manera visual en la consola.
+
+## Tecnologías utilizadas
+* **Lenguaje:** Python 3.14.2
+* **Librerías:** * `BeautifulSoup4` para (Parsing de HTML)
+    * `Requests` para (Peticiones HTTP)
+    * `Pandas` para (Estructuración de datos)
+
+## Requisitos e Instalación
+1. **Clonar el repositorio:**
+   * git clone [https://github.com/TU_USUARIO/cve-sentinel-scraper.git](https://github.com/TU_USUARIO/cve-sentinel-scraper.git)
+
+2. **Crear y activar entorno virtual**
+    * python -m venv venv
+    * .\venv\Scripts\activate
+
+3. **Instalar Dependencias**
+    * pip install -r requirements.txt
+
+
+Desarrollado por: Frank Isai Morales
---
+++ scraper.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+
+def safe_scrape():
+    url = "https://seclists.org/oss-sec/2026/q1/" 
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0' # Para que el servidor no nos bloquee, nos hacemos pasar por un navegador cualquiera
+    }
+
+    print(f"[*] Connected To The Url: {url}")
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status() 
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # En esta página, los reportes están en enlaces dentro de una lista
+        Links = soup.find_all('a', href=True)
+        
+        palabras_criticas = ["CRITICAL", "RCE", "EXPLOIT", "REMOTE", "CODE EXECUTION"]
+
+        data_report = []
+        
+        for link in Links:
+            texto = link.text.strip()
+            texto_upper = texto.upper()
+            # Filtramos solo aquellos que parecen reportes de vulnerabilidades (contienen CVE o nombres de software)
+            if any(keyword in texto.upper() for keyword in ["CVE", "ADVISORY", "SECURITY", "VULN"]):
+                its_Critical = "No"
+                if any(crit in texto_upper for crit in palabras_criticas):
+                    its_Critical = "Yes - Check"
+
+                data_report.append({
+                    "Report_Title": texto,
+                    "Critical": its_Critical,
+                    "Refer_Link": f"https://seclists.org/oss-sec/2026/q1/{link['href']}"
+                })
+
+        return data_report
+
+    except Exception as e:
+        print(f"[!] Error: {e}")
+        return []
+
+if __name__ == "__main__":
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+    results = safe_scrape()
+
+    if results:
+        # Tomamos 100
+        df = pd.DataFrame(results[:]) 
+        df.to_csv("data/inteligencia_oss.csv", index=True)
+        print(f"[+] Success. {len(df)} Data storaged in inteligencia_oss.csv.")
+        print("[+] Data saved in: data/inteligencia_oss.csv")
+
+        alerts = df[df["Critical"] == "Yes - Check"]
+        
+        print("\n--- Last Checked Security Reports ---") 
+        print(df["Report_Title"].head(5))
+    else:
+        print("[-] No New Reports Detected")
